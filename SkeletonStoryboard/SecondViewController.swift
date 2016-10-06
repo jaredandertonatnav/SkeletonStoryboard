5d2aa67c11dfcaf4e4848b2ebec3548f4534302c
Jared Anderton <janderton@nav.com>
2016-10-06 12:08:23 -0600

    initial commit

---
+++ SkeletonStoryboard/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  SkeletonStoryboard
+//
+//  Created by janderton on 10/6/16.
+//  Copyright © 2016 Nav Technologies. All rights reserved.
+//
+
+import UIKit
+class SecondViewController: UIViewController {
+
+}
--- SkeletonStoryboard/ViewController.swift
+++ SkeletonStoryboard/ViewController.swift
@@ -7,19 +7,12 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBAction func nextButtonTapped(_ sender: AnyObject) {
+        self.performSegue(withIdentifier: "Next", sender: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
